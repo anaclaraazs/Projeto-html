91d04a00c7086485923fcd3d707ccdb23c9f9624
anaclaraazs <claraka91@gmail.com>
2024-06-20 09:00:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let numero1 = document.querySelector('#entrada1');
+let numero2 = document.querySelector('#entrada2');
+let resultado = document.querySelector('#resultado');
+
+function somar(){
+    resultado.value = numero1.value + numero2.value;  
+}
